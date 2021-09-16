269f916ff8b30c5c255d7942a3a651307739e0a5
unknown <slisette127@gmail.com>
2021-09-15 22:06:15 -0500

    final push

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor () {
-    super()
+  
 
-    this.state = {
-      isClicked: false
+    state = {
+      isClicked: true, 
+      inputValue: "",
+      listOfTodos: []
     }
+  
+
+  handleClick = () => {
+    this.state.isClicked ? 
+      this.setState({isClicked : false }) : 
+        this.setState({isClicked : true })
   }
+
+  handleChange = (event) => {
+    this.setState({inputValue: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({listOfTodos: [...this.state.listOfTodos, this.state.inputValue]})
+    this.setState({inputValue: ""})
+  }
+
   render() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sandra
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.inputValue} onChange={this.handleChange}></input>
+       <button type="submit">Submit</button>
+      </form>
+
+        <ol>{this.state.listOfTodos.map((todo, index) => {
+          return <li key={index}>{todo}</li>
+        })}</ol>
+       
       </header>
     </div>
     );
